d4b100807055ae63cbcf002f06a697beac528e84
SilverCrown <54302245+AstroAntics@users.noreply.github.com>
2021-05-21 04:57:27 +0530

    Update nukepm.php

--- includes/nukepm.php
+++ includes/nukepm.php
@@ -2,8 +2,27 @@
 //Boilerplate template for deleting PMs.
 
 $id = $_POST['pm_id'];
+$mode = $_POST['mode'];
 
-if (isset($id)
+//Deleting all PMs
+if (isset($mode) && $mode === "all")
+{
+        $query = "UPDATE $pm_table SET deleted=1 WHERE recipient = '$recipient'";
+        $result = $db->query($query) or die ('mysql_error())');
+        if ($result) 
+        {  
+                echo "Your inbox has been cleared successfully. Returning to inbox..."; 
+                header ("Location: /pmbox.php");
+        } 
+        
+        else 
+        { 
+                die("Sorry, we couldn't clear your inbox. Please try again."); 
+        }
+}
+
+//Deleting a single PM
+if (isset($id))
 {
         $query = "UPDATE $pm_table SET deleted = 1 WHERE pm_id = '$id'";
         $result = $db->query($query) or die('mysql_error())');
